1811a68f20f08a045e872a21267d8262a3b0b3b4
Cameron Irwin <cameron.irwinp@gmail.com>
2014-07-24 14:14:02 -0700

    Created new project and made part of first screens

---
+++ app/src/main/java/com/example/camirwin/invoicetracker/ClientsActivity.java
@@ -0,0 +1,39 @@
+package com.example.camirwin.invoicetracker;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ClientsActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clients);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.clients, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else if (id == R.id.action_add_client) {
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
